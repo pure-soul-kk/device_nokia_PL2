e09769c9df10d0cb4e4d6c3bf73b9ec742cb57d2
pure-soul-kk <krishnakripa34567@gmail.com>
2023-06-25 09:57:36 +0530

    [tmp]PL2: track external/tinycompress from lineage-20

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -17,3 +17,7 @@ git clone https://github.com/ProjectElixir-Devices/kernel_nokia_sdm660.git kerne
 # Clang
 rm -rf prebuilts/clang/kernel/linux-x86/clang-r416183b
 git clone https://github.com/pure-soul-kk/android_prebuilts_clang_kernel_linux-x86_clang-r416183b.git prebuilts/clang/kernel/linux-x86/clang-r416183b
+
+# Tinycompress
+rm -rf external/tinycompress
+git clone https://github.com/LineageOS/android_external_tinycompress external/tinycompress
